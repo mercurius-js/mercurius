5f8419eab35e259f20e4425cd6f1d3f72047ff8a
Wojciech Trocki <wtrocki@redhat.com>
2019-06-11 23:29:59 +0100

    fix: Comprehensive tests for query filtering feature

--- test/queryFields.js
+++ test/queryFields.js
@@ -2,14 +2,40 @@
 
 const { test } = require('tap')
 const info = require('./mocks/infoBasic.json')
+const fragment = require('./mocks/infoFragment.json')
+const inline = require('./mocks/infoInline.json')
 const { buildQueryObject, getQueryFields } = require('../queryFields')
 
-test('test getQueryFields with relationships', async (t) => {
+test('test getQueryFields with relationship', async (t) => {
   const result = getQueryFields(info)
   t.deepEqual(result, {
-    fields: ['name'],
+    fields: ['title'],
     relations: {
-      'mother': [
+      'category': [
+        'name'
+      ]
+    }
+  })
+})
+
+test('test getQueryFields with relationship on inline fragment', async (t) => {
+  const result = getQueryFields(fragment)
+  t.deepEqual(result, {
+    fields: ['title'],
+    relations: {
+      'category': [
+        'name'
+      ]
+    }
+  })
+})
+
+test('test getQueryFields with relationship on fragment', async (t) => {
+  const result = getQueryFields(inline)
+  t.deepEqual(result, {
+    fields: ['title'],
+    relations: {
+      'category': [
         'name'
       ]
     }
@@ -18,7 +44,16 @@ test('test getQueryFields with relationships', async (t) => {
 
 test('test buildQueryObject', async (t) => {
   const query = buildQueryObject(info)
-  t.strictEqual(query.hasRelation('mother'), true)
-  t.deepEqual(query.getRelationFields('mother'), '"name" as "mother__name"')
-  t.deepEqual(query.getRootFields(), '"name"')
+  t.strictEqual(query.hasRelation('category'), true)
+  t.deepEqual(query.getRelationFields('category'), '"name" as "category__name"')
+  t.deepEqual(query.getRootFields(), '"title"')
+})
+
+test('test expandToGraph', async (t) => {
+  const query = buildQueryObject(info)
+  const graph = query.expandToGraph([{
+    'category__name': 'test'
+  }], ['category'])
+
+  t.deepEqual(graph[0].category, { 'name': 'test' })
 })
