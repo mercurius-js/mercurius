39810fbc6c9a7ac7b6e5cda64b93454f0da05f62
John Behan <john.behan@nearform.com>
2019-08-23 10:30:02 +0100

    setup base test for query depth

--- test/query-depth.js
+++ test/query-depth.js
@@ -1,116 +1,152 @@
-// 'use strict'
+'use strict'
 
-// const { test } = require('tap')
-// const Fastify = require('fastify')
-// const GQL = require('..')
+const { test } = require('tap')
+const Fastify = require('fastify')
+const GQL = require('..')
 
-// const dogs = [{
-//   name: 'Max',
-//   owner: 'Jennifer',
-//   breed: 'Labrador'
-// }, {
-//   name: 'Charlie',
-//   owner: 'Sarah',
-//   breed: 'Labradoodle'
-// }, {
-//   name: 'Buddy',
-//   owner: 'Tracy',
-//   breed: 'Labrasatian'
-// }]
+const dogs = [{
+  name: 'Max',
+  owner: 'Jennifer',
+  breed: 'Labrador'
+}, {
+  name: 'Charlie',
+  owner: 'Sarah',
+  breed: 'Labradoodle'
+}, {
+  name: 'Buddy',
+  owner: 'Tracy',
+  breed: 'Labrasatian'
+}]
 
-// const owners = [
-//   {
-//     name: 'Jennifer',
-//     pet: 'Max'
-//   },
-//   {
-//     name: 'Sarah',
-//     pet: 'Charlie'
-//   },
-//   {
-//     name: 'Tracy',
-//     pet: 'Buddy'
-//   }
-// ]
+const owners = [
+  {
+    name: 'Jennifer',
+    pet: 'Max'
+  },
+  {
+    name: 'Sarah',
+    pet: 'Charlie'
+  },
+  {
+    name: 'Tracy',
+    pet: 'Buddy'
+  }
+]
 
-// const schema = `
-//   type Human {
-//     name: String!
-//     pet: Dog
-//   }
+const schema = `
+  type Human {
+    name: String!
+    pet: Dog
+  }
 
-//   type Dog {
-//     name: String!
-//     owner: Human
-//   }
+  type Dog {
+    name: String!
+    owner: Human
+  }
 
-//   type Query {
-//     dogs: [Dog]
-//   }
-// `
+  type Query {
+    dogs: [Dog]
+  }
+`
 
-// const resolvers = {
-//   Human: {
-//     pet (human, params, { reply }) {
-//       return dogs.find(dog => dog.owner === human.name)
-//     }
-//   },
-//   Dog: {
-//     human (dog, params, { reply }) {
-//       return owners.find(owner => owner.pet === dog.name)
-//     }
-//   },
-//   Query: {
-//     dogs (_, params, { reply }) {
-//       return dogs
-//     }
-//   }
-// }
+const resolvers = {
+  Human: {
+    pet (human, params, { reply }) {
+      return dogs.find(dog => dog.name === human.pet)
+    }
+  },
+  Dog: {
+    owner (dog, params, { reply }) {
+      return owners.find(owner => owner.pet === dog.name)
+    }
+  },
+  Query: {
+    dogs (_, params, { reply }) {
+      return dogs
+    }
+  }
+}
 
-// const query = `{
-//   dogs {
-//     name,
-//     owner {
-//       name
-//     }
-//   }
-// }`
+const query = `{
+  dogs {
+    name
+    owner {
+      name
+      pet {
+        name
+        owner {
+          name
+          pet {
+            name
+          }
+        }
+      }
+    }
+  }
+}`
 
-// test('queryDepth', async (t) => {
-//   const app = Fastify()
+test('queryDepth', async (t) => {
+  const app = Fastify()
 
-//   app.register(GQL, {
-//     schema,
-//     resolvers
-//   })
+  app.register(GQL, {
+    schema,
+    resolvers
+  })
 
-//   // needed so that graphql is defined
-//   await app.ready()
+  // needed so that graphql is defined
+  await app.ready()
 
-//   const res = await app.graphql(query)
+  const res = await app.graphql(query)
 
-//   console.log(JSON.parse(res.body))
-
-//   t.deepEqual(res, {
-//     data: [
-//       {
-//         name: 'Max',
-//         owner: {
-//           name: 'Jennifer'
-//         }
-//       },
-//       {
-//         name: 'Charlie',
-//         owner: {
-//           name: 'Sarah'
-//         }
-//       },
-//       {
-//         name: 'Buddy',
-//         owner: {
-//           name: 'Tracy'
-//         }
-//       }
-//     ]
-//   })
-// })
+  t.deepEqual(res, {
+    data: {
+      dogs: [
+        {
+          name: 'Max',
+          owner: {
+            name: 'Jennifer',
+            pet: {
+              name: 'Max',
+              owner: {
+                name: 'Jennifer',
+                pet: {
+                  name: 'Max'
+                }
+              }
+            }
+          }
+        },
+        {
+          name: 'Charlie',
+          owner: {
+            name: 'Sarah',
+            pet: {
+              name: 'Charlie',
+              owner: {
+                name: 'Sarah',
+                pet: {
+                  name: 'Charlie'
+                }
+              }
+            }
+          }
+        },
+        {
+          name: 'Buddy',
+          owner: {
+            name: 'Tracy',
+            pet: {
+              name: 'Buddy',
+              owner: {
+                name: 'Tracy',
+                pet: {
+                  name: 'Buddy'
+                }
+              }
+            }
+          }
+        }
+      ]
+    }
+  })
+})
