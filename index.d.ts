884c3382f14acbd5e906a376aad48ec719566b0b
Peter Balazs <frikille@gmail.com>
2019-10-15 17:24:54 +0100

    feat: add typings (#76)
    * add typescript typings
    
    * Add @types/graphql to devDependencies
    
    * exclude node_modules for typescript compilation
    
    * fix @types/graphql version to 14.2.3
    
    * implement review feedbacks
    
    * fix prefix

---
+++ index.d.ts
@@ -0,0 +1,144 @@
+import fastify, {
+  FastifyError,
+  FastifyReply,
+  FastifyRequest,
+  RegisterOptions
+} from "fastify"
+import { IResolvers } from "graphql-tools";
+import { Server, IncomingMessage, ServerResponse } from "http";
+import { Http2Server, Http2ServerRequest, Http2ServerResponse } from 'http2';
+import graphql, { GraphQLSchema, GraphQLError, Source, DocumentNode, ExecutionResult } from 'graphql';
+
+declare namespace FastifyGQL {
+
+  export interface Plugin<HttpResponse> {
+    /**
+     * Extend existing schema
+     * @param schema graphql schema
+     */
+    extendSchema(schema: string | Source | DocumentNode): void;
+    /**
+     * Define additional resolvers
+     * @param resolvers object with resolver functions
+     */
+    defineResolvers(resolvers: IResolvers): void;
+    /**
+     * Define data loaders
+     * @param loaders object with data loader functions
+     */
+    defineLoaders(loaders: {
+      [key: string]: {
+        [key: string]: (
+          queries: Array<{
+            obj: any,
+            params: any 
+          }>,
+          context: {
+            reply: FastifyReply<HttpResponse>
+          }) => any
+      }
+    }): void;
+  }
+
+  export interface Options<
+    HttpServer extends (Server | Http2Server),
+    HttpRequest extends (IncomingMessage | Http2ServerRequest),
+    HttpResponse extends (ServerResponse | Http2ServerResponse)
+  > extends RegisterOptions<HttpServer, HttpRequest, HttpResponse> {
+    /**
+     * The GraphQL schema. String schema will be parsed
+     */
+    schema: GraphQLSchema | string,
+    /**
+     * Object with resolver functions
+     */
+    resolvers: IResolvers,
+    /**
+     * Object with data loader functions
+     */
+    loaders?: {
+      [key: string]: {
+        [key: string]: (
+          queries: Array<{
+            obj: any,
+            params: any 
+          }>,
+          context: {
+            reply: FastifyReply<HttpResponse>
+          }) => any
+      }
+    },
+    /**
+     * Serve GraphiQL on /graphiql if true or 'graphiql', or GraphQL IDE on /playground if 'playground' if routes is true
+     */
+    graphiql?: boolean | string,
+    /**
+     * The minimum number of execution a query needs to be executed before being jit'ed.
+     * @default true
+     */
+    jit?: number,
+    /**
+     * A graphql endpoint is exposed at /graphql when true
+     * @default true
+     */
+    routes?: boolean,
+    /**
+     * Change the route prefix of the graphql endpoint if set
+     */
+    prefix?: string,
+    /**
+     * Add the empty Mutation definition if schema is not defined
+     * @default false
+     */
+    defineMutation?: boolean,
+    /**
+     * Change the default error handler (Default: true).
+     * If a custom error handler is defined, it should return the standardized response format according to [GraphQL spec](https://graphql.org/learn/serving-over-http/#response).
+     * @default true
+     */
+    errorHandler?: boolean | ((
+      error: FastifyError,
+      request: FastifyRequest<HttpRequest>,
+      reply: FastifyReply<HttpResponse>
+    ) => ExecutionResult),
+    /**
+     * The maximum depth allowed for a single query.
+     */
+    queryDepth?: number
+  }
+}
+
+declare module "fastify" {
+  interface FastifyInstance<HttpServer, HttpRequest, HttpResponse> {
+    /**
+     * GraphQL plugin
+     */
+    graphql: FastifyGQL.Plugin<HttpResponse>;
+  }
+
+  interface FastifyReply<HttpResponse> {
+    /**
+     * @param source GraphQL query string
+     * @param context request context
+     * @param variables request variables which will get passed to the executor
+     * @param operationName specify which operation will be run
+     */
+      graphql(
+        source: string,
+        context?: any,
+        variables?: { [key: string]: any },
+        operationName?: string
+      ): FastifyReply<HttpResponse>;
+  }
+}
+
+declare function fastifyGQL<
+  HttpServer extends (Server | Http2Server),
+  HttpRequest extends (IncomingMessage | Http2ServerRequest),
+  HttpResponse extends (ServerResponse | Http2ServerResponse), 
+  Options = FastifyGQL.Options<HttpServer, HttpRequest, HttpResponse>
+> (
+  fastify: fastify.FastifyInstance<HttpServer, HttpRequest, HttpResponse>,
+  opts: Options) : void;
+
+export = fastifyGQL;
---
+++ test/types/index.ts
@@ -0,0 +1,89 @@
+import Fastify from 'fastify'
+import GQL from '../..'
+
+const app = Fastify()
+
+const dogs = [{
+  name: 'Max'
+}, {
+  name: 'Charlie'
+}, {
+  name: 'Buddy'
+}, {
+  name: 'Max'
+}]
+
+const owners = {
+  Max: {
+    name: 'Jennifer'
+  },
+  Charlie: {
+    name: 'Sarah'
+  },
+  Buddy: {
+    name: 'Tracy'
+  }
+}
+
+const schema = `
+  type Query {
+    add(x: Int, y: Int): Int
+  }
+`
+
+const resolvers = {
+  Query: {
+    add: async (_: any, { x, y }: { x: number, y: number }) => x + y
+  }
+}
+
+app.register(GQL, {
+  schema: schema,
+  resolvers,
+  loaders: {},
+  graphiqls: true,
+  jit: 1,
+  routes: true,
+  prefix: '/prefix',
+  defineMutation: false,
+  errorHandler: true,
+  queryDepth: 8
+})
+
+app.register(async function (app) {
+  app.graphql.extendSchema(`
+  type Human {
+    name: String!
+  }
+
+  type Dog {
+    name: String!
+    owner: Human
+  }
+
+  type Query {
+    dogs: [Dog]
+  }
+  `)
+  app.graphql.defineResolvers({
+    Query: {
+      dogs (_, params, { reply }) {
+        return dogs
+      }
+    }
+  })
+  app.graphql.defineLoaders({
+    Dog: {
+      async owner (queries: Array<{ obj: { name: keyof typeof owners } }>) {
+        return queries.map(({ obj }) => owners[obj.name])
+      }
+    }
+  })
+})
+
+app.get('/', async function (req, reply) {
+  const query = '{ add(x: 2, y: 2) }'
+  return reply.graphql(query)
+})
+
+app.listen(3000)
