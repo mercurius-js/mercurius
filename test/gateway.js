fe719ed744484f59ea37bbaac6849ea381896821
Peter Balazs <frikille@gmail.com>
2020-03-30 20:34:40 +0100

    resolve field by different service

--- test/gateway.js
+++ test/gateway.js
@@ -10,231 +10,232 @@ async function createService (t, port, schema, resolvers = {}) {
   service.register(GQL, {
     schema,
     resolvers,
-    federationMetadata: true
+    federationMetadata: true,
+    graphiql: true
   })
   await service.listen(port)
 }
 
-test('"schema" option not allowed in gateway moode', async (t) => {
-  const app = Fastify()
-  const schema = `
-    type Query {
-      add(x: Int, y: Int): Int
-    }
-  `
+// test('"schema" option not allowed in gateway moode', async (t) => {
+//   const app = Fastify()
+//   const schema = `
+//     type Query {
+//       add(x: Int, y: Int): Int
+//     }
+//   `
 
-  app.register(GQL, {
-    schema,
-    gateway: {
-      services: []
-    }
-  })
+//   app.register(GQL, {
+//     schema,
+//     gateway: {
+//       services: []
+//     }
+//   })
 
-  try {
-    await app.ready()
-  } catch (err) {
-    t.is(err.message, 'Adding "schema", "resolvers", "loaders" or "subscription" to plugin options when plugin is running in gateway mode is not allowed')
-  }
-})
+//   try {
+//     await app.ready()
+//   } catch (err) {
+//     t.is(err.message, 'Adding "schema", "resolvers", "loaders" or "subscription" to plugin options when plugin is running in gateway mode is not allowed')
+//   }
+// })
 
-test('"resolvers" option not allowed in gateway moode', async (t) => {
-  const app = Fastify()
+// test('"resolvers" option not allowed in gateway moode', async (t) => {
+//   const app = Fastify()
 
-  app.register(GQL, {
-    resolvers: {},
-    gateway: {
-      services: []
-    }
-  })
+//   app.register(GQL, {
+//     resolvers: {},
+//     gateway: {
+//       services: []
+//     }
+//   })
 
-  try {
-    await app.ready()
-  } catch (err) {
-    t.is(err.message, 'Adding "schema", "resolvers", "loaders" or "subscription" to plugin options when plugin is running in gateway mode is not allowed')
-  }
-})
+//   try {
+//     await app.ready()
+//   } catch (err) {
+//     t.is(err.message, 'Adding "schema", "resolvers", "loaders" or "subscription" to plugin options when plugin is running in gateway mode is not allowed')
+//   }
+// })
 
-test('"loaders" option not allowed in gateway moode', async (t) => {
-  const app = Fastify()
+// test('"loaders" option not allowed in gateway moode', async (t) => {
+//   const app = Fastify()
 
-  app.register(GQL, {
-    loaders: {},
-    gateway: {
-      services: []
-    }
-  })
+//   app.register(GQL, {
+//     loaders: {},
+//     gateway: {
+//       services: []
+//     }
+//   })
 
-  try {
-    await app.ready()
-  } catch (err) {
-    t.is(err.message, 'Adding "schema", "resolvers", "loaders" or "subscription" to plugin options when plugin is running in gateway mode is not allowed')
-  }
-})
+//   try {
+//     await app.ready()
+//   } catch (err) {
+//     t.is(err.message, 'Adding "schema", "resolvers", "loaders" or "subscription" to plugin options when plugin is running in gateway mode is not allowed')
+//   }
+// })
 
-test('"subscription" option not allowed in gateway moode', async (t) => {
-  const app = Fastify()
+// test('"subscription" option not allowed in gateway moode', async (t) => {
+//   const app = Fastify()
 
-  app.register(GQL, {
-    subscription: true,
-    gateway: {
-      services: []
-    }
-  })
+//   app.register(GQL, {
+//     subscription: true,
+//     gateway: {
+//       services: []
+//     }
+//   })
 
-  try {
-    await app.ready()
-  } catch (err) {
-    t.is(err.message, 'Adding "schema", "resolvers", "loaders" or "subscription" to plugin options when plugin is running in gateway mode is not allowed')
-  }
-})
+//   try {
+//     await app.ready()
+//   } catch (err) {
+//     t.is(err.message, 'Adding "schema", "resolvers", "loaders" or "subscription" to plugin options when plugin is running in gateway mode is not allowed')
+//   }
+// })
 
-test('calling defineLoaders throws an error in gateway mode', async (t) => {
-  await createService(t, 3001, `
-    extend type Query {
-      me: User
-    }
+// test('calling defineLoaders throws an error in gateway mode', async (t) => {
+//   await createService(t, 3001, `
+//     extend type Query {
+//       me: User
+//     }
 
-    type User @key(fields: "id") {
-      id: ID!
-      name: String!
-    }
-  `)
+//     type User @key(fields: "id") {
+//       id: ID!
+//       name: String!
+//     }
+//   `)
 
-  const app = Fastify()
-  t.tearDown(() => app.close())
+//   const app = Fastify()
+//   t.tearDown(() => app.close())
 
-  app.register(GQL, {
-    gateway: {
-      services: [{
-        name: 'service-1',
-        url: 'http://localhost:3001/graphql'
-      }]
-    }
-  })
+//   app.register(GQL, {
+//     gateway: {
+//       services: [{
+//         name: 'service-1',
+//         url: 'http://localhost:3001/graphql'
+//       }]
+//     }
+//   })
 
-  await app.ready()
+//   await app.ready()
 
-  try {
-    app.graphql.defineLoaders({
-      Query: {
-        field () {}
-      }
-    })
-  } catch (err) {
-    t.is(err.message, 'Calling defineLoaders method is not allowed when plugin is running in gateway mode is not allowed')
-  }
-})
+//   try {
+//     app.graphql.defineLoaders({
+//       Query: {
+//         field () {}
+//       }
+//     })
+//   } catch (err) {
+//     t.is(err.message, 'Calling defineLoaders method is not allowed when plugin is running in gateway mode is not allowed')
+//   }
+// })
 
-test('calling defineResolvers throws an error in gateway mode', async (t) => {
-  await createService(t, 3001, `
-    extend type Query {
-      me: User
-    }
+// test('calling defineResolvers throws an error in gateway mode', async (t) => {
+//   await createService(t, 3001, `
+//     extend type Query {
+//       me: User
+//     }
 
-    type User @key(fields: "id") {
-      id: ID!
-      name: String!
-    }
-  `)
+//     type User @key(fields: "id") {
+//       id: ID!
+//       name: String!
+//     }
+//   `)
 
-  const app = Fastify()
-  t.tearDown(() => app.close())
+//   const app = Fastify()
+//   t.tearDown(() => app.close())
 
-  app.register(GQL, {
-    gateway: {
-      services: [{
-        name: 'service-1',
-        url: 'http://localhost:3001/graphql'
-      }]
-    }
-  })
+//   app.register(GQL, {
+//     gateway: {
+//       services: [{
+//         name: 'service-1',
+//         url: 'http://localhost:3001/graphql'
+//       }]
+//     }
+//   })
 
-  await app.ready()
+//   await app.ready()
 
-  try {
-    app.graphql.defineResolvers({
-      Query: {
-        field () {}
-      }
-    })
-  } catch (err) {
-    t.is(err.message, 'Calling defineResolvers method is not allowed when plugin is running in gateway mode is not allowed')
-  }
-})
+//   try {
+//     app.graphql.defineResolvers({
+//       Query: {
+//         field () {}
+//       }
+//     })
+//   } catch (err) {
+//     t.is(err.message, 'Calling defineResolvers method is not allowed when plugin is running in gateway mode is not allowed')
+//   }
+// })
 
-test('calling replaceSchema throws an error in gateway mode', async (t) => {
-  await createService(t, 3001, `
-    extend type Query {
-      me: User
-    }
+// test('calling replaceSchema throws an error in gateway mode', async (t) => {
+//   await createService(t, 3001, `
+//     extend type Query {
+//       me: User
+//     }
 
-    type User @key(fields: "id") {
-      id: ID!
-      name: String!
-    }
-  `)
+//     type User @key(fields: "id") {
+//       id: ID!
+//       name: String!
+//     }
+//   `)
 
-  const app = Fastify()
-  t.tearDown(() => app.close())
+//   const app = Fastify()
+//   t.tearDown(() => app.close())
 
-  app.register(GQL, {
-    gateway: {
-      services: [{
-        name: 'service-1',
-        url: 'http://localhost:3001/graphql'
-      }]
-    }
-  })
+//   app.register(GQL, {
+//     gateway: {
+//       services: [{
+//         name: 'service-1',
+//         url: 'http://localhost:3001/graphql'
+//       }]
+//     }
+//   })
 
-  await app.ready()
+//   await app.ready()
 
-  try {
-    app.graphql.replaceSchema(`
-      type Query {
-        field: String!
-      }
-    `)
-  } catch (err) {
-    t.is(err.message, 'Calling replaceSchema method is not allowed when plugin is running in gateway mode is not allowed')
-  }
-})
+//   try {
+//     app.graphql.replaceSchema(`
+//       type Query {
+//         field: String!
+//       }
+//     `)
+//   } catch (err) {
+//     t.is(err.message, 'Calling replaceSchema method is not allowed when plugin is running in gateway mode is not allowed')
+//   }
+// })
 
-test('calling extendSchema throws an error in gateway mode', async (t) => {
-  await createService(t, 3001, `
-    extend type Query {
-      me: User
-    }
+// test('calling extendSchema throws an error in gateway mode', async (t) => {
+//   await createService(t, 3001, `
+//     extend type Query {
+//       me: User
+//     }
 
-    type User @key(fields: "id") {
-      id: ID!
-      name: String!
-    }
-  `)
+//     type User @key(fields: "id") {
+//       id: ID!
+//       name: String!
+//     }
+//   `)
 
-  const app = Fastify()
-  t.tearDown(() => app.close())
+//   const app = Fastify()
+//   t.tearDown(() => app.close())
 
-  app.register(GQL, {
-    gateway: {
-      services: [{
-        name: 'service-1',
-        url: 'http://localhost:3001/graphql'
-      }]
-    }
-  })
+//   app.register(GQL, {
+//     gateway: {
+//       services: [{
+//         name: 'service-1',
+//         url: 'http://localhost:3001/graphql'
+//       }]
+//     }
+//   })
 
-  await app.ready()
+//   await app.ready()
 
-  try {
-    app.graphql.extendSchema(`
-      extend type Query {
-        field: String!
-      }
-    `)
-  } catch (err) {
-    t.is(err.message, 'Calling extendSchema method is not allowed when plugin is running in gateway mode is not allowed')
-  }
-})
+//   try {
+//     app.graphql.extendSchema(`
+//       extend type Query {
+//         field: String!
+//       }
+//     `)
+//   } catch (err) {
+//     t.is(err.message, 'Calling extendSchema method is not allowed when plugin is running in gateway mode is not allowed')
+//   }
+// })
 
 test('It builds the gateway schema correctly', async (t) => {
   const users = {
@@ -311,14 +312,21 @@ test('It builds the gateway schema correctly', async (t) => {
     }
   `, {
     Post: {
-      __resolveReference: (parent, args, context, info) => {
-        return posts.filter(p => p.author === parent.id)
+      __resolveReference: (post, args, context, info) => {
+        console.log('resolvereference')
+        return posts[post.id]
       },
       author: (post, args, context, info) => {
         return {
           __typename: 'User',
           id: post.authorId
         }
+      }
+    },
+    User: {
+      posts: (user, args, context, info) => {
+        console.log('hello')
+        return Object.values(posts).filter(p => p.authorId === user.id)
       }
     }
   })
@@ -339,7 +347,7 @@ test('It builds the gateway schema correctly', async (t) => {
 
   await gateway.listen(3000)
 
-  const query = '{ me { id name posts { id title content author { id } } } }'
+  const query = '{ me { id name posts { id title content } } }'
   const res = await gateway.inject({
     method: 'GET',
     url: `/graphql?query=${query}`
@@ -350,7 +358,15 @@ test('It builds the gateway schema correctly', async (t) => {
       me: {
         id: 'u1',
         name: 'John',
-        posts: null
+        posts: [{
+          id: 'p1',
+          title: 'Post 1',
+          content: 'Content 1'
+        }, {
+          id: 'p3',
+          title: 'Post 3',
+          content: 'Content 3'
+        }]
       }
     }
   })
