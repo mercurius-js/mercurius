369d1613bb798b68bb7454fa2e13df9f04098651
Jonathan Steven Tanuwidjaja <yondercode@gmail.com>
2020-05-02 12:20:07 +0700

    Added more tests

--- test/errors.js
+++ test/errors.js
@@ -5,64 +5,25 @@ const Fastify = require('fastify')
 const GQL = require('..')
 const { FastifyGraphQLError } = require('../lib/errors')
 
-const schema = `
-  type Query {
-    errorOne: String
-    errorTwo: String
-  }
-`
-
-const resolvers = {
-  Query: {
-    errorOne () {
-      throw new FastifyGraphQLError('Error One', 'ERROR_ONE', { additional: 'information one', other: 'data one' })
-    },
-    errorTwo () {
-      throw new FastifyGraphQLError('Error Two', 'ERROR_TWO', { additional: 'information two', other: 'data two' })
+test('errors - multiple custom errors', async (t) => {
+  const schema = `
+    type Query {
+      errorOne: String
+      errorTwo: String
     }
-  }
-}
+  `
 
-const goodPayload = {
-  data: {
-    errorOne: null,
-    errorTwo: null
-  },
-  errors: [
-    {
-      message: 'Error One',
-      locations: [
-        {
-          line: 1,
-          column: 2
-        }
-      ],
-      path: ['errorOne'],
-      extensions: {
-        code: 'ERROR_ONE',
-        additional: 'information one',
-        other: 'data one'
-      }
-    },
-    {
-      message: 'Error Two',
-      locations: [
-        {
-          line: 1,
-          column: 11
-        }
-      ],
-      path: ['errorTwo'],
-      extensions: {
-        code: 'ERROR_TWO',
-        additional: 'information two',
-        other: 'data two'
+  const resolvers = {
+    Query: {
+      errorOne () {
+        throw new FastifyGraphQLError('Error One', 'ERROR_ONE', { additional: 'information one', other: 'data one' })
+      },
+      errorTwo () {
+        throw new FastifyGraphQLError('Error Two', 'ERROR_TWO', { additional: 'information two', other: 'data two' })
       }
     }
-  ]
-}
+  }
 
-test('errors - test custom errors', async (t) => {
   const app = Fastify()
 
   app.register(GQL, {
@@ -79,5 +40,97 @@ test('errors - test custom errors', async (t) => {
   })
 
   t.equal(res.statusCode, 200)
-  t.deepEqual(JSON.parse(res.payload), goodPayload)
+  t.deepEqual(JSON.parse(res.payload), {
+    data: {
+      errorOne: null,
+      errorTwo: null
+    },
+    errors: [
+      {
+        message: 'Error One',
+        locations: [
+          {
+            line: 1,
+            column: 2
+          }
+        ],
+        path: ['errorOne'],
+        extensions: {
+          code: 'ERROR_ONE',
+          additional: 'information one',
+          other: 'data one'
+        }
+      },
+      {
+        message: 'Error Two',
+        locations: [
+          {
+            line: 1,
+            column: 11
+          }
+        ],
+        path: ['errorTwo'],
+        extensions: {
+          code: 'ERROR_TWO',
+          additional: 'information two',
+          other: 'data two'
+        }
+      }
+    ]
+  })
+})
+
+test('errors - custom errors with number additionalProperties', async (t) => {
+  const schema = `
+    type Query {
+      willThrow: String
+    }
+  `
+
+  const resolvers = {
+    Query: {
+      willThrow () {
+        throw new FastifyGraphQLError('Custom Error', 'CUSTOM_ERROR', { floating: 3.14, timestamp: 1324356 })
+      }
+    }
+  }
+
+  const app = Fastify()
+
+  app.register(GQL, {
+    schema,
+    resolvers
+  })
+
+  // needed so that graphql is defined
+  await app.ready()
+
+  const res = await app.inject({
+    method: 'GET',
+    url: '/graphql?query={willThrow}'
+  })
+
+  t.equal(res.statusCode, 200)
+  t.deepEqual(JSON.parse(res.payload), {
+    data: {
+      willThrow: null
+    },
+    errors: [
+      {
+        message: 'Custom Error',
+        locations: [
+          {
+            line: 1,
+            column: 2
+          }
+        ],
+        path: ['willThrow'],
+        extensions: {
+          code: 'CUSTOM_ERROR',
+          floating: 3.14,
+          timestamp: 1324356
+        }
+      }
+    ]
+  })
 })
