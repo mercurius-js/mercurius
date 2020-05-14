aa3afc790233f1e431c78e2a6c7706fc9e284e6f
The Half Blood Prince <thehalfbloodprince.github@gmail.com>
2020-05-14 13:21:52 +0530

    Fixes missing error extensions when query is jit'ed (#165)
    * fix: error extensions not being populated when query is jit'ed
    
    * chore: update tests
    
    * chore: update docs
    
    * chore: refactor tests and add test with jit enabled
    
    Co-authored-by: tHBp <thehalflbloodprince.github@gmail.com>

--- test/errors.js
+++ test/errors.js
@@ -8,19 +8,15 @@ const { ErrorWithProps } = GQL
 test('errors - multiple extended errors', async (t) => {
   const schema = `
     type Query {
-      errorOne: String
-      errorTwo: String
+      error: String
       successful: String
     }
   `
 
   const resolvers = {
     Query: {
-      errorOne () {
-        throw new ErrorWithProps('Error One', 'ERROR_ONE', { additional: 'information one', other: 'data one' })
-      },
-      errorTwo () {
-        throw new ErrorWithProps('Error Two', 'ERROR_TWO', { additional: 'information two', other: 'data two' })
+      error () {
+        throw new ErrorWithProps('Error', { code: 'ERROR', additional: 'information', other: 'data' })
       },
       successful () {
         return 'Runs OK'
@@ -39,52 +35,36 @@ test('errors - multiple extended errors', async (t) => {
 
   const res = await app.inject({
     method: 'GET',
-    url: '/graphql?query={errorOne,errorTwo, successful}'
+    url: '/graphql?query={error,successful}'
   })
 
   t.equal(res.statusCode, 200)
   t.deepEqual(JSON.parse(res.payload), {
     data: {
-      errorOne: null,
-      errorTwo: null,
+      error: null,
       successful: 'Runs OK'
     },
     errors: [
       {
-        message: 'Error One',
+        message: 'Error',
         locations: [
           {
             line: 1,
             column: 2
           }
         ],
-        path: ['errorOne'],
+        path: ['error'],
         extensions: {
-          code: 'ERROR_ONE',
-          additional: 'information one',
-          other: 'data one'
-        }
-      },
-      {
-        message: 'Error Two',
-        locations: [
-          {
-            line: 1,
-            column: 11
-          }
-        ],
-        path: ['errorTwo'],
-        extensions: {
-          code: 'ERROR_TWO',
-          additional: 'information two',
-          other: 'data two'
+          code: 'ERROR',
+          additional: 'information',
+          other: 'data'
         }
       }
     ]
   })
 })
 
-test('errors - extended errors with number additionalProperties', async (t) => {
+test('errors - extended errors with number extensions', async (t) => {
   const schema = `
     type Query {
       willThrow: String
@@ -94,7 +74,7 @@ test('errors - extended errors with number additionalProperties', async (t) => {
   const resolvers = {
     Query: {
       willThrow () {
-        throw new ErrorWithProps('Extended Error', 'EXTENDED_ERROR', { floating: 3.14, timestamp: 1324356, reason: 'some reason' })
+        throw new ErrorWithProps('Extended Error', { code: 'EXTENDED_ERROR', floating: 3.14, timestamp: 1324356, reason: 'some reason' })
       }
     }
   }
@@ -155,13 +135,7 @@ test('errors - extended errors optional parameters', async (t) => {
         throw new ErrorWithProps('Extended Error')
       },
       two () {
-        throw new ErrorWithProps('Extended Error', 'ERROR_TWO')
-      },
-      three () {
-        throw new ErrorWithProps('Extended Error', 'ERROR_THREE', { reason: 'some reason' })
-      },
-      four () {
-        throw new ErrorWithProps('Extended Error', undefined, { reason: 'some reason' })
+        throw new ErrorWithProps('Extended Error', { code: 'ERROR_TWO', reason: 'some reason' })
       }
     }
   }
@@ -177,16 +151,14 @@ test('errors - extended errors optional parameters', async (t) => {
 
   const res = await app.inject({
     method: 'GET',
-    url: '/graphql?query={one,two,three,four}'
+    url: '/graphql?query={one,two}'
   })
 
   t.equal(res.statusCode, 200)
   t.deepEqual(JSON.parse(res.payload), {
     data: {
       one: null,
-      two: null,
-      three: null,
-      four: null
+      two: null
     },
     errors: [
       {
@@ -197,8 +169,7 @@ test('errors - extended errors optional parameters', async (t) => {
             column: 2
           }
         ],
-        path: ['one'],
-        extensions: {}
+        path: ['one']
       },
       {
         message: 'Extended Error',
@@ -210,34 +181,73 @@ test('errors - extended errors optional parameters', async (t) => {
         ],
         path: ['two'],
         extensions: {
-          code: 'ERROR_TWO'
+          code: 'ERROR_TWO',
+          reason: 'some reason'
         }
+      }
+    ]
+  })
+})
+
+test('errors - errors with jit enabled', async (t) => {
+  const schema = `
+    type Query {
+      error: String
+      successful: String
+    }
+  `
+
+  const resolvers = {
+    Query: {
+      error () {
+        throw new ErrorWithProps('Error', { code: 'ERROR', additional: 'information', other: 'data' })
       },
+      successful () {
+        return 'Runs OK'
+      }
+    }
+  }
+
+  const app = Fastify()
+
+  app.register(GQL, {
+    schema,
+    resolvers,
+    jit: 1
+  })
+
+  await app.ready()
+
+  await app.inject({
+    method: 'GET',
+    url: '/graphql?query={error,successful}'
+  })
+
+  const res = await app.inject({
+    method: 'GET',
+    url: '/graphql?query={error,successful}'
+  })
+
+  t.equal(res.statusCode, 200)
+  t.deepEqual(JSON.parse(res.payload), {
+    data: {
+      error: null,
+      successful: 'Runs OK'
+    },
+    errors: [
       {
-        message: 'Extended Error',
+        message: 'Error',
         locations: [
           {
             line: 1,
-            column: 10
+            column: 2
           }
         ],
-        path: ['three'],
+        path: ['error'],
         extensions: {
-          code: 'ERROR_THREE',
-          reason: 'some reason'
-        }
-      },
-      {
-        message: 'Extended Error',
-        locations: [
-          {
-            line: 1,
-            column: 16
-          }
-        ],
-        path: ['four'],
-        extensions: {
-          reason: 'some reason'
+          code: 'ERROR',
+          additional: 'information',
+          other: 'data'
         }
       }
     ]
