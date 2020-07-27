a48bb27c212d7de8b7adfac7c49fe5adf2ae3d99
Paco Dupont <paco.dupont@gmail.com>
2020-07-27 15:10:22 +0200

    feat: handle subscription socket has stream (not working)

--- test/subscription-connection.js
+++ test/subscription-connection.js
@@ -2,6 +2,7 @@
 const { test } = require('tap')
 const proxyquire = require('proxyquire')
 const WebSocket = require('ws')
+const { Duplex } = require('stream')
 const fastify = require('fastify')
 const mq = require('mqemitter')
 const SubscriptionConnection = require('../lib/subscription-connection')
@@ -18,7 +19,7 @@ test('socket is closed on unhandled promise rejection in handleMessage', t => {
 
     handleConnectionClose () {
       handleConnectionCloseCalled = true
-      this.socket.close()
+      this.connection.destroy()
     }
   }
 
@@ -62,26 +63,27 @@ test('socket is closed on unhandled promise rejection in handleMessage', t => {
 })
 
 test('subscripction connection sends error message when message is not json string', async (t) => {
-  const sc = new SubscriptionConnection({
-    on () {},
-    send (message) {
-      t.equal(JSON.stringify({
-        type: 'error',
-        id: null,
-        payload: 'Message must be a JSON string'
-      }), message)
-    }
-  }, {})
+  const connection = new Duplex()
+  connection._read = function () {}
+  connection._write = function (message) {
+    t.equal(JSON.stringify({
+      type: 'error',
+      id: null,
+      payload: 'Message must be a JSON string'
+    }), message.toString())
+  }
+
+  const sc = new SubscriptionConnection(connection, {})
 
   await sc.handleMessage('invalid json string')
 })
 
 test('subscription connection handles GQL_CONNECTION_TERMINATE message correctly', async (t) => {
-  const sc = new SubscriptionConnection({
-    on () {},
-    close () { t.pass() },
-    send (message) {}
-  }, {})
+  const connection = new Duplex()
+  connection._read = function () { }
+  connection._write = function (message) {}
+
+  const sc = new SubscriptionConnection(connection, {})
 
   await sc.handleMessage(JSON.stringify({
     id: 1,
@@ -91,11 +93,12 @@ test('subscription connection handles GQL_CONNECTION_TERMINATE message correctly
 
 test('subscription connection handles GQL_STOP message correctly', async (t) => {
   t.plan(2)
-  const sc = new SubscriptionConnection({
-    on () {},
-    close () {},
-    send (message) {}
-  }, {})
+
+  const connection = new Duplex()
+  connection._read = function () { }
+  connection._write = function (message) { }
+
+  const sc = new SubscriptionConnection(connection, {})
 
   sc.subscriptionContexts = new Map()
   sc.subscriptionContexts.set(1, {
@@ -113,28 +116,26 @@ test('subscription connection handles GQL_STOP message correctly', async (t) => 
 })
 
 test('handles error in send and closes connection', async t => {
-  const sc = new SubscriptionConnection(
-    {
-      send (message) {
-        throw new Error('Socket closed')
-      },
-      close () {
-        t.pass()
-      },
-      on () {}
-    },
-    {}
-  )
+  const connection = new Duplex()
+  connection._read = function () { }
+  connection._write = function (message) {
+    throw new Error('Socket closed')
+  }
+  connection.on('finish', function () {
+    t.pass()
+  })
+
+  const sc = new SubscriptionConnection(connection, {})
 
   await sc.sendMessage('foo')
 })
 
 test('subscription connection handles GQL_STOP message correctly, with no data', async (t) => {
-  const sc = new SubscriptionConnection({
-    on () {},
-    close () {},
-    send (message) {}
-  }, {})
+  const connection = new Duplex()
+  connection._read = function () { }
+  connection._write = function (message) { }
+
+  const sc = new SubscriptionConnection(connection, {})
 
   await sc.handleMessage(JSON.stringify({
     id: 1,
@@ -145,17 +146,17 @@ test('subscription connection handles GQL_STOP message correctly, with no data',
 })
 
 test('subscription connection send error message when GQL_START handler errs', async (t) => {
-  const sc = new SubscriptionConnection({
-    on () {},
-    close () {},
-    send (message) {
-      t.equal(JSON.stringify({
-        id: 1,
-        type: 'error',
-        payload: 'handleGQLStart error'
-      }), '{"id":1,"type":"error","payload":"handleGQLStart error"}')
-    }
-  }, {})
+  const connection = new Duplex()
+  connection._read = function () { }
+  connection._write = function (message) {
+    t.equal(JSON.stringify({
+      id: 1,
+      type: 'error',
+      payload: 'handleGQLStart error'
+    }), '{"id":1,"type":"error","payload":"handleGQLStart error"}')
+  }
+
+  const sc = new SubscriptionConnection(connection, {})
 
   sc.handleGQLStart = async (data) => {
     throw new Error('handleGQLStart error')
@@ -169,17 +170,17 @@ test('subscription connection send error message when GQL_START handler errs', a
 })
 
 test('subscription connection send error message when client message type is invalid', async (t) => {
-  const sc = new SubscriptionConnection({
-    on () {},
-    close () {},
-    send (message) {
-      t.equal(JSON.stringify({
-        id: 1,
-        type: 'error',
-        payload: 'Invalid payload type'
-      }), '{"id":1,"type":"error","payload":"Invalid payload type"}')
-    }
-  }, {})
+  const connection = new Duplex()
+  connection._read = function () { }
+  connection._write = function (message) {
+    t.equal(JSON.stringify({
+      id: 1,
+      type: 'error',
+      payload: 'Invalid payload type'
+    }), '{"id":1,"type":"error","payload":"Invalid payload type"}')
+  }
+
+  const sc = new SubscriptionConnection(connection, {})
 
   await sc.handleMessage(JSON.stringify({
     id: 1,
@@ -189,11 +190,11 @@ test('subscription connection send error message when client message type is inv
 })
 
 test('subscription connection handles GQL_START message correctly, when payload.query is not defined', async (t) => {
-  const sc = new SubscriptionConnection({
-    on () {},
-    close () {},
-    send (message) {}
-  }, {})
+  const connection = new Duplex()
+  connection._read = function () { }
+  connection._write = function (message) { }
+
+  const sc = new SubscriptionConnection(connection, {})
 
   await sc.handleMessage(JSON.stringify({
     id: 1,
