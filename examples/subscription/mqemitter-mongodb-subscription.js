0c1f6ef6f3017565a3386ec40bba999e6703c427
Santiago Almeida Bolaños <santiagodeveloper666@gmail.com>
2020-08-15 14:18:38 +0300

    add mqemitter mongodb subscription example (#81) (#250)

---
+++ examples/subscription/mqemitter-mongodb-subscription.js
@@ -0,0 +1,90 @@
+'use strict'
+
+const GQL = require('fastify-gql')
+const Fastify = require('fastify')
+const mongodbMQEmitter = require('mqemitter-mongodb')
+
+const app = Fastify({ logger: true })
+
+// mq
+let emitter
+
+// list of products
+const products = []
+
+// graphql schema
+const schema = `
+  type Product {
+    name: String!
+    state: String!
+  }
+
+  type Query {
+    products: [Product]
+  }
+
+  type Mutation {
+    addProduct(name: String!, state: String!): Product
+  }
+
+  type Subscription {
+    productAdded: Product
+  }
+`
+
+// graphql resolvers
+const resolvers = {
+  Query: {
+    products: () => products
+  },
+  Mutation: {
+    addProduct: async (_, { name, state }, { pubsub }) => {
+      const product = { name, state }
+
+      products.push(product)
+
+      pubsub.publish({
+        topic: 'new_product_updates',
+        payload: {
+          productAdded: product
+        }
+      })
+
+      return product
+    }
+  },
+  Subscription: {
+    productAdded: {
+      subscribe: async (_, __, { pubsub }) => {
+        return await pubsub.subscribe('new_product_updates')
+      }
+    }
+  }
+}
+
+const handle = (conn) => conn.pipe(conn)
+
+// server start
+const start = async () => {
+  try {
+    // initialize emitter
+    emitter = mongodbMQEmitter({ url: 'mongodb://localhost/test' })
+
+    // register GraphQl
+    app.register(GQL, {
+      schema,
+      resolvers,
+      subscription: {
+        emitter,
+        handle
+      }
+    })
+
+    // start server
+    await app.listen(3000)
+  } catch (error) {
+    app.log.error(error)
+  }
+}
+
+start()
