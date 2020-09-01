56ae4be303955ce6cfc634e6a7ea1648d851c6d2
Anthony Ringoet <anthonyringoet@gmail.com>
2020-09-01 07:24:20 +0200

    Fix ./bench.sh

--- bench.sh
+++ bench.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./node_modules/.bin/autocannon -c 100 -d 5 -p 10 --on-port '/graphql?query={add(x:2,y:2)}' -- node example.js
+./node_modules/.bin/autocannon -c 100 -d 5 -p 10 --on-port '/graphql?query={add(x:2,y:2)}' -- node examples/basic.js
