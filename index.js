7ce03840eef3232e844ac6ff3ff13ee2af769383
RafaelGSS <rafael.nunu@hotmail.com>
2020-09-14 22:29:01 -0300

    fix(validationErrors): call errorFormatter on validationErrors

--- index.js
+++ index.js
@@ -390,7 +390,7 @@ const plugin = fp(async function (app, opts) {
         }
         const err = new BadRequest()
         err.errors = validationErrors
-        throw err
+        return maybeFormatErrors(err, reply)
       }
 
       if (queryDepthLimit) {
