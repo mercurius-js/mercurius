757d83bd8d7e4cee3af1b91623dd4663400c2204
Paco Dupont <paco.dupont@exwexs.fr>
2020-10-14 11:11:46 +0200

    feat(gateway): add initHeaders option for services

--- test/gateway/schema.js
+++ test/gateway/schema.js
@@ -1127,3 +1127,52 @@ test('Should support array references with _entities query and empty response an
     }
   })
 })
+
+test('Gateway sends initHeaders with _service sdl query', async (t) => {
+  t.plan(1)
+  const service = Fastify()
+  t.tearDown(() => {
+    service.close()
+  })
+  service.register(GQL, {
+    schema: `
+      extend type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: async () => {
+          return 'world'
+        }
+      }
+    },
+    federationMetadata: true
+  })
+  service.addHook('preHandler', async (req, reply) => {
+    t.equal(req.headers.authorization, 'ok')
+    if (!req.headers.authorization) throw new Error('Unauthorized')
+  })
+
+  await service.listen(0)
+
+  const gateway = Fastify()
+  t.tearDown(() => {
+    gateway.close()
+  })
+  gateway.register(GQL, {
+    gateway: {
+      services: [
+        {
+          name: 'svc',
+          url: `http://localhost:${service.server.address().port}/graphql`,
+          initHeaders: {
+            authorization: 'ok'
+          }
+        }
+      ]
+    }
+  })
+
+  await gateway.ready()
+})
