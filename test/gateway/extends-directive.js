347b4b3b4481e6590861c7a054130f30f7148cdf
Jonny Green <31290910+projectjudge@users.noreply.github.com>
2020-12-05 20:00:50 +0000

    Add logic to strip non-service types on queries containing inline fragments (#357)

--- test/gateway/extends-directive.js
+++ test/gateway/extends-directive.js
@@ -306,3 +306,169 @@ test('gateway passes field arguments through to types labeled by @extends direct
     }
   })
 })
+
+test('gateway distributes query correctly to services when querying with inline fragments', async (t) => {
+  const userPosts = {
+    p1: {
+      pid: 'p1',
+      title: 'Post 1',
+      content: 'Content 1',
+      authorId: 'u1'
+    },
+    p2: {
+      pid: 'p2',
+      title: 'Post 2',
+      content: 'Content 2',
+      authorId: 'u2'
+    },
+    p3: {
+      pid: 'p3',
+      title: 'Post 3',
+      content: 'Content 3',
+      authorId: 'u1'
+    },
+    p4: {
+      pid: 'p4',
+      title: 'Post 4',
+      content: 'Content 4',
+      authorId: 'u1'
+    }
+  }
+
+  const userServicePort = await createService(t, `
+    type Query @extends {
+      me: UserUnion
+    }
+
+    type User @key(fields: "id") {
+      id: ID!
+      name: String!
+    }
+
+    type Admin {
+      id: ID!
+    }
+
+    union UserUnion = User | Admin
+  `, {
+    Query: {
+      me: (root, args, context, info) => {
+        return users.u1
+      }
+    },
+    User: {
+      __resolveReference: (user, args, context, info) => {
+        return users[user.id]
+      }
+    }
+  })
+
+  const postServicePort = await createService(t, `
+    type Post @key(fields: "pid") {
+      pid: ID!
+      author: User
+    }
+
+    type User @key(fields: "id") @extends {
+      id: ID! @external
+      topPosts(count: Int!): [Post]
+    }
+  `, {
+    Post: {
+      __resolveReference: (post, args, context, info) => {
+        return userPosts[post.pid]
+      },
+      author: (post, args, context, info) => {
+        return {
+          __typename: 'User',
+          id: post.authorId
+        }
+      }
+    },
+    User: {
+      topPosts: (user, { count }, context, info) => {
+        return Object.values(userPosts).filter(p => p.authorId === user.id).slice(0, count)
+      }
+    }
+  })
+
+  const gateway = Fastify()
+  t.tearDown(() => {
+    gateway.close()
+  })
+  gateway.register(GQL, {
+    gateway: {
+      services: [{
+        name: 'user',
+        url: `http://localhost:${userServicePort}/graphql`
+      }, {
+        name: 'post',
+        url: `http://localhost:${postServicePort}/graphql`
+      }]
+    }
+  })
+
+  await gateway.listen(0)
+
+  const query = `
+    query {
+      me {
+        ... on User {
+          id
+          name
+          topPosts(count: 2) {
+            pid
+            author {
+              id
+              topPosts(count: 1) {
+                pid
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const res = await gateway.inject({
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    url: '/graphql',
+    body: JSON.stringify({ query })
+  })
+
+  t.deepEqual(JSON.parse(res.body), {
+    data: {
+      me: {
+        id: 'u1',
+        name: 'John',
+        topPosts: [
+          {
+            pid: 'p1',
+            author: {
+              id: 'u1',
+              topPosts: [
+                {
+                  pid: 'p1'
+                }
+              ]
+            }
+          },
+          {
+            pid: 'p3',
+            author: {
+              id: 'u1',
+              topPosts: [
+                {
+                  pid: 'p1'
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  })
+})
