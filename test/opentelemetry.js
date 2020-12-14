f52a850706840470a20c90e9d96290a8793c279b
Paco Dupont <paco.dupont@exwexs.fr>
2020-12-14 11:37:11 +0100

    test(opentelemetry): refine span context testing

--- test/opentelemetry.js
+++ test/opentelemetry.js
@@ -7,6 +7,11 @@ const GQL = require('..')
 test('Should add opentelemetry span and cached attribute', async (t) => {
   t.plan(6)
 
+  const testSpanContext = {
+    traceId: 'd4cda95b652f4a1592b449d5929fda1b',
+    spanId: '6e0c63257de34c92',
+    traceFlags: api.TraceFlags.NONE
+  }
   class TestSpan extends api.NoopSpan {
     setAttribute (name, value) {
       t.is(name, 'mercurius.cached')
@@ -17,11 +22,16 @@ test('Should add opentelemetry span and cached attribute', async (t) => {
       t.pass()
     }
   }
+  const dummySpan = new TestSpan(testSpanContext)
   class TestTracer extends api.NoopTracer {
     startSpan (name, opts) {
       t.is(name, 'mercurius - graphql')
-      t.deepEquals(opts, { parent: new api.NoopSpan() })
-      return new TestSpan()
+      t.deepEquals(opts.parent.context(), dummySpan.context())
+      return new TestSpan(testSpanContext)
+    }
+
+    getCurrentSpan () {
+      return dummySpan
     }
   }
   class TestTracerProvider extends api.NoopTracerProvider {
