9647d2f049f854a11658e0aec04ff01c1379371e
Jonny Green <hello@jonnydgreen.com>
2021-02-25 00:54:47 +0000

    Add subscription hooks to Mercurius (#407)

---
+++ examples/hooks-subscription.js
@@ -0,0 +1,80 @@
+'use strict'
+
+const Fastify = require('fastify')
+const mq = require('mqemitter')
+const mercurius = require('..')
+
+async function start () {
+  const app = Fastify()
+
+  const schema = `
+    type Notification {
+      id: ID!
+      message: String
+    }
+
+    type Mutation {
+      addNotification(message: String): Notification
+    }
+
+    type Subscription {
+      notificationAdded: Notification
+    }
+  `
+
+  const emitter = mq()
+
+  const resolvers = {
+    Mutation: {
+      addNotification: async (_, { message }) => {
+        const notification = {
+          id: 1,
+          message
+        }
+        await emitter.emit({
+          topic: 'NOTIFICATION_ADDED',
+          payload: {
+            notificationAdded: notification
+          }
+        })
+
+        return notification
+      }
+    },
+    Subscription: {
+      notificationAdded: {
+        subscribe: (root, args, { pubsub }) => pubsub.subscribe('NOTIFICATION_ADDED')
+      }
+    }
+  }
+
+  app.register(mercurius, {
+    schema,
+    resolvers,
+    subscription: {
+      emitter
+    }
+  })
+
+  await app.ready()
+
+  app.graphql.addHook('preSubscriptionParsing', async function (schema, source, context) {
+    console.log('preSubscriptionParsing called')
+  })
+
+  app.graphql.addHook('preSubscriptionExecution', async function (schema, document, context) {
+    console.log('preSubscriptionExecution called')
+  })
+
+  app.graphql.addHook('onSubscriptionResolution', async function (execution, context) {
+    console.log('onSubscriptionResolution called')
+  })
+
+  app.graphql.addHook('onSubscriptionEnd', async function (context) {
+    console.log('onSubscriptionEnd called')
+  })
+
+  await app.listen(3000)
+}
+
+start()
---
+++ test/subscription-hooks.js
@@ -0,0 +1,402 @@
+'use strict'
+
+const { test } = require('tap')
+const Fastify = require('fastify')
+const WebSocket = require('ws')
+const mq = require('mqemitter')
+const { once } = require('events')
+const { GraphQLSchema, parse } = require('graphql')
+const GQL = require('..')
+
+function sendTestMutation (app) {
+  app.inject({
+    method: 'POST',
+    url: '/graphql',
+    body: {
+      query: `
+        mutation {
+          addNotification(message: "Hello World") {
+            id
+          }
+        }
+      `
+    }
+  }, () => {})
+}
+
+const query = `subscription {
+  notificationAdded {
+    id
+    message
+  }
+}`
+
+function createTestServer (t) {
+  const app = Fastify()
+  t.tearDown(() => app.close())
+
+  const emitter = mq()
+
+  const schema = `
+    type Notification {
+      id: ID!
+      message: String
+    }
+
+    type Query {
+      notifications: [Notification]
+    }
+
+    type Mutation {
+      addNotification(message: String): Notification
+    }
+
+    type Subscription {
+      notificationAdded: Notification
+    }
+  `
+
+  const resolvers = {
+    Mutation: {
+      addNotification: async (_, { message }) => {
+        const notification = {
+          id: 1,
+          message
+        }
+        await emitter.emit({
+          topic: 'NOTIFICATION_ADDED',
+          payload: {
+            notificationAdded: notification
+          }
+        })
+
+        return notification
+      }
+    },
+    Subscription: {
+      notificationAdded: {
+        subscribe: (root, args, { pubsub }) => pubsub.subscribe('NOTIFICATION_ADDED')
+      }
+    }
+  }
+
+  app.register(GQL, {
+    schema,
+    resolvers,
+    subscription: {
+      emitter
+    }
+  })
+
+  return app
+}
+
+function createWebSocketClient (t, app) {
+  const ws = new WebSocket('ws://localhost:' + (app.server.address()).port + '/graphql', 'graphql-ws')
+  const client = WebSocket.createWebSocketStream(ws, { encoding: 'utf8', objectMode: true })
+  t.tearDown(client.destroy.bind(client))
+  client.setEncoding('utf8')
+  return { client, ws }
+}
+
+test('subscription - hooks basic', async t => {
+  t.plan(13)
+
+  const app = await createTestServer(t)
+
+  app.graphql.addHook('preSubscriptionParsing', async (schema, source, context) => {
+    t.type(schema, GraphQLSchema)
+    t.is(source, query)
+    t.type(context, 'object')
+    t.ok('preSubscriptionParsing called')
+  })
+  app.graphql.addHook('preSubscriptionExecution', async (schema, document, context) => {
+    t.type(schema, GraphQLSchema)
+    t.deepEqual(document, parse(query))
+    t.type(context, 'object')
+    t.ok('preSubscriptionExecution called')
+  })
+  app.graphql.addHook('preGatewaySubscriptionExecution', async (schema, document, context) => {
+    t.fail('preGatewaySubscriptionExecution should not be called in non-gateway mode')
+  })
+  app.graphql.addHook('onSubscriptionResolution', async (execution, context) => {
+    t.deepEqual(execution, {
+      data: {
+        notificationAdded: {
+          id: '1',
+          message: 'Hello World'
+        }
+      }
+    })
+    t.type(context, 'object')
+    t.ok('onSubscriptionResolution called')
+  })
+
+  await app.listen(0)
+
+  const { client } = createWebSocketClient(t, app)
+
+  client.write(JSON.stringify({
+    type: 'connection_init'
+  }))
+  client.write(JSON.stringify({
+    id: 1,
+    type: 'start',
+    payload: {
+      query
+    }
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.is(data.type, 'connection_ack')
+  }
+
+  sendTestMutation(app)
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.deepEqual(data, {
+      id: 1,
+      type: 'data',
+      payload: {
+        data: {
+          notificationAdded: {
+            id: '1',
+            message: 'Hello World'
+          }
+        }
+      }
+    })
+  }
+})
+
+// -----------------------
+// preSubscriptionParsing
+// -----------------------
+test('subscription - should handle preSubscriptionParsing hook errors', async t => {
+  t.plan(2)
+  const app = await createTestServer(t)
+
+  app.graphql.addHook('preSubscriptionParsing', async (schema, source, context) => {
+    throw new Error('a preSubscriptionParsing error occurred')
+  })
+  app.graphql.addHook('preSubscriptionParsing', async (schema, document, context) => {
+    t.fail('preSubscriptionParsing should not be called again')
+  })
+  app.graphql.addHook('preSubscriptionExecution', async (schema, document, context) => {
+    t.fail('preSubscriptionExecution should not be called')
+  })
+  app.graphql.addHook('onSubscriptionResolution', async (execution, context) => {
+    t.fail('onSubscriptionResolution should not be called')
+  })
+
+  await app.listen(0)
+
+  const { client } = createWebSocketClient(t, app)
+
+  client.write(JSON.stringify({
+    type: 'connection_init'
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.is(data.type, 'connection_ack')
+  }
+
+  client.write(JSON.stringify({
+    id: 1,
+    type: 'start',
+    payload: {
+      query
+    }
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.deepEqual(data, {
+      id: 1,
+      type: 'error',
+      payload: 'a preSubscriptionParsing error occurred'
+    })
+  }
+})
+
+// -----------------------
+// preSubscriptionExecution
+// -----------------------
+test('subscription - should handle preSubscriptionExecution hook errors', async t => {
+  t.plan(2)
+  const app = await createTestServer(t)
+
+  app.graphql.addHook('preSubscriptionExecution', async (schema, document, context) => {
+    throw new Error('a preSubscriptionExecution error occurred')
+  })
+  app.graphql.addHook('preSubscriptionExecution', async (execution, context) => {
+    t.fail('preSubscriptionExecution should not be called again')
+  })
+  app.graphql.addHook('onSubscriptionResolution', async (execution, context) => {
+    t.fail('onSubscriptionResolution should not be called')
+  })
+
+  await app.listen(0)
+
+  const { client } = createWebSocketClient(t, app)
+
+  client.write(JSON.stringify({
+    type: 'connection_init'
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.is(data.type, 'connection_ack')
+  }
+
+  client.write(JSON.stringify({
+    id: 1,
+    type: 'start',
+    payload: {
+      query
+    }
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.deepEqual(data, {
+      id: 1,
+      type: 'error',
+      payload: 'a preSubscriptionExecution error occurred'
+    })
+  }
+})
+
+// -----------------------
+// onSubscriptionResolution
+// -----------------------
+test('subscription - should handle onSubscriptionResolution hook errors', async t => {
+  t.plan(2)
+  const app = await createTestServer(t)
+
+  app.graphql.addHook('onSubscriptionResolution', async (execution, context) => {
+    throw new Error('kaboom')
+  })
+
+  app.graphql.addHook('onSubscriptionResolution', async (execution, context) => {
+    t.fail('onSubscriptionResolution should not be called agin')
+  })
+
+  await app.listen(0)
+
+  const { client, ws } = createWebSocketClient(t, app)
+
+  client.write(JSON.stringify({
+    type: 'connection_init'
+  }))
+  client.write(JSON.stringify({
+    id: 1,
+    type: 'start',
+    payload: {
+      query: query
+    }
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.is(data.type, 'connection_ack')
+  }
+
+  sendTestMutation(app)
+
+  await once(client, 'end')
+  t.is(ws.readyState, WebSocket.CLOSED)
+})
+
+// -----------------
+// onSubscriptionEnd
+// -----------------
+test('subscription - should call onSubscriptionEnd when subscription ends', async t => {
+  t.plan(4)
+  const app = await createTestServer(t)
+
+  app.graphql.addHook('onSubscriptionEnd', async (context) => {
+    t.type(context, 'object')
+    t.ok('onSubscriptionEnd called')
+  })
+
+  await app.listen(0)
+
+  const { client } = createWebSocketClient(t, app)
+
+  client.write(JSON.stringify({
+    type: 'connection_init'
+  }))
+  client.write(JSON.stringify({
+    id: 1,
+    type: 'start',
+    payload: {
+      query: query
+    }
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.is(data.type, 'connection_ack')
+  }
+
+  client.write(JSON.stringify({
+    id: 1,
+    type: 'stop'
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.is(data.type, 'complete')
+  }
+})
+
+test('subscription - should handle onSubscriptionEnd hook errors', async t => {
+  t.plan(2)
+  const app = await createTestServer(t)
+
+  app.graphql.addHook('onSubscriptionEnd', async (context) => {
+    throw new Error('kaboom')
+  })
+
+  await app.listen(0)
+
+  const { client, ws } = createWebSocketClient(t, app)
+
+  client.write(JSON.stringify({
+    type: 'connection_init'
+  }))
+  client.write(JSON.stringify({
+    id: 1,
+    type: 'start',
+    payload: {
+      query: query
+    }
+  }))
+
+  {
+    const [chunk] = await once(client, 'data')
+    const data = JSON.parse(chunk)
+    t.is(data.type, 'connection_ack')
+  }
+
+  client.write(JSON.stringify({
+    id: 1,
+    type: 'stop'
+  }))
+
+  await once(client, 'end')
+  t.is(ws.readyState, WebSocket.CLOSED)
+})
