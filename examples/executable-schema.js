0e207b498921bde55e0ae2f6c5ac3bc127b5932e
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2021-03-08 20:47:14 +0200

    docs: Link to Examples folder added (#411)
    * Update README.md
    
    * Update sidebar.md
    
    * Update sidebar.md
    
    * Update README.md
    
    * Executable Schema moved to Examples folder

---
+++ examples/executable-schema.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const Fastify = require('fastify')
+const mercurius = require('mercurius')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+
+const app = Fastify()
+
+const typeDefs = `
+  type Query {
+    add(x: Int, y: Int): Int
+  }
+`
+
+const resolvers = {
+  Query: {
+    add: async (_, { x, y }) => x + y
+  }
+}
+
+app.register(mercurius, {
+  schema: makeExecutableSchema({ typeDefs, resolvers })
+})
+
+app.get('/', async function (req, reply) {
+  const query = '{ add(x: 2, y: 2) }'
+  return reply.graphql(query)
+})
+
+app.listen(3000)
