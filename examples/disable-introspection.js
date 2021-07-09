e0c1f1a4adc88799f8beb687e930406c5273a1b8
Lucas Bittencurt <bittencurt.lucas@protonmail.com>
2021-07-09 00:33:33 -0300

    validate the disable instrospection rule from graphql inside the validationRule api

---
+++ examples/disable-introspection.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Fastify = require('fastify')
+const mercurius = require('..')
+const graphql = require('graphql')
+
+const app = Fastify()
+
+const schema = `
+  type Query {
+    add(x: Int, y: Int): Int
+  }
+`
+
+const resolvers = {
+  Query: {
+    add: async (_, obj) => {
+      const { x, y } = obj
+      return x + y
+    }
+  }
+}
+
+app.register(mercurius, {
+  schema,
+  resolvers,
+  graphiql: true,
+  validationRules: [graphql.NoSchemaIntrospectionCustomRule]
+})
+
+app.get('/', async function (req, reply) {
+  const query = '{ add(x: 2, y: 2) }'
+  return reply.graphql(query)
+})
+
+app.listen(3000)
