bc2b1a442340cb951a089e63d95c89ac45f904df
Florian Traverse <174621+temsa@users.noreply.github.com>
2022-05-03 17:44:04 +0200

    fix: gateway configuration validation (#789)

--- index.js
+++ index.js
@@ -151,6 +151,25 @@ const plugin = fp(async function (app, opts) {
     throw new MER_ERR_INVALID_OPTS('Adding "schema", "resolvers" or "loaders" to plugin options when plugin is running in gateway mode is not allowed')
   }
 
+  if (gateway && Array.isArray(gateway.services)) {
+    const serviceNames = new Set()
+    for (const service of gateway.services) {
+      if (typeof service !== 'object') {
+        throw new MER_ERR_INVALID_OPTS('gateway: all "services" must be objects')
+      }
+      if (typeof service.name !== 'string') {
+        throw new MER_ERR_INVALID_OPTS('gateway: all "services" must have a "name" String property')
+      }
+      if (serviceNames.has(service.name)) {
+        throw new MER_ERR_INVALID_OPTS(`gateway: all "services" must have a unique "name": "${service.name}" is already used`)
+      }
+      serviceNames.add(service.name)
+      if (typeof service.url !== 'string' && (!Array.isArray(service.url) || service.url.length === 0 || !service.url.every(url => typeof url === 'string'))) {
+        throw new MER_ERR_INVALID_OPTS('gateway: all "services" must have an "url" String, or a non-empty Array of String, property')
+      }
+    }
+  }
+
   if (Array.isArray(schema)) {
     schema = schema.join('\n')
   }
