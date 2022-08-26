6f05624a1d18735a967d4e30953f10e673f8e9d6
Drakhart <14996002+drakhart@users.noreply.github.com>
2022-08-26 14:26:07 +0200

    fix: add missing ts properties

--- index.d.ts
+++ index.d.ts
@@ -25,6 +25,9 @@ export interface PubSub {
 export interface MercuriusContext {
   app: FastifyInstance;
   reply: FastifyReply;
+  operationsCount?: Number;
+  operationId?: Number;
+  __currentQuery: String;
   /**
    * __Caution__: Only available if `subscriptions` are enabled
    */
