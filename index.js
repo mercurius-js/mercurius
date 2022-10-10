d29a4233add792b28ddcd760c85bb0a06d982789
Davide Fiorello <davide@codeflyer.com>
2022-10-10 17:39:06 +0200

    feat(gateway): move validation in gateway file

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const {
 const { buildExecutionContext } = require('graphql/execution/execute')
 const queryDepth = require('./lib/queryDepth')
 const buildFederationSchema = require('./lib/federation')
-const buildGateway = require('./lib/gateway/build-gateway')
+const { buildGateway, validateGateway } = require('./lib/gateway')
 const mq = require('mqemitter')
 const { PubSub, withFilter } = require('./lib/subscriber')
 const persistedQueryDefaults = require('./lib/persistedQueryDefaults')
@@ -157,27 +157,8 @@ const plugin = fp(async function (app, opts) {
     fastifyGraphQl.pubsub = subscriber
   }
 
-  if (gateway && (schema || opts.resolvers || opts.loaders)) {
-    throw new MER_ERR_INVALID_OPTS('Adding "schema", "resolvers" or "loaders" to plugin options when plugin is running in gateway mode is not allowed')
-  }
-
-  if (gateway && Array.isArray(gateway.services)) {
-    const serviceNames = new Set()
-    for (const service of gateway.services) {
-      if (typeof service !== 'object') {
-        throw new MER_ERR_INVALID_OPTS('gateway: all "services" must be objects')
-      }
-      if (typeof service.name !== 'string') {
-        throw new MER_ERR_INVALID_OPTS('gateway: all "services" must have a "name" String property')
-      }
-      if (serviceNames.has(service.name)) {
-        throw new MER_ERR_INVALID_OPTS(`gateway: all "services" must have a unique "name": "${service.name}" is already used`)
-      }
-      serviceNames.add(service.name)
-      if (typeof service.url !== 'string' && (!Array.isArray(service.url) || service.url.length === 0 || !service.url.every(url => typeof url === 'string'))) {
-        throw new MER_ERR_INVALID_OPTS('gateway: all "services" must have an "url" String, or a non-empty Array of String, property')
-      }
-    }
+  if (gateway) {
+    validateGateway(schema, gateway, opts)
   }
 
   if (Array.isArray(schema)) {
