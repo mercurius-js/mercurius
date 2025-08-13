2fd7ed238d52e47cfbffcdc90a09e117469ea072
Tony133 <a.tripodi133@gmail.com>
2025-08-13 15:13:56 +0200

    fix: update errors.js test

--- test/errors.js
+++ test/errors.js
@@ -890,8 +890,9 @@ test('bad json', async (t) => {
 
   t.equal(res.statusCode, 400)
   if (semver.gte(process.version, '20.0.0')) {
+    console.log(res.json())
     t.same(res.json(),
-      { data: null, errors: [{ message: 'Unexpected token \'h\', "this is not a json" is not valid JSON' }] }
+      { data: null, errors: [{ message: "Body is not valid JSON but content-type is set to 'application/json'" }] }
     )
   } else {
     t.same(res.json(),
